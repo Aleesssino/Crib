9177a1ed08f46faf6113314e306b4db116852c01
Aleš Gábor <alesg3a1b4o1@gmail.com>
2023-07-05 00:22:22 +0200

    not so atrocious button

--- src/components/postForm.tsx
+++ src/components/postForm.tsx
@@ -18,7 +18,7 @@ function Form () {
     return (
         <form className="flex border-b px-4 py-2">
             <textarea placeholder="What's on your mind?" className="bg-transparent flex w-full rounded-xl p-4 text-xl grow border-b-2 overflow-hidden"/>
-            <button className="ml-1 px-4 py-2 h-24 w-10 border-l border-b border-orange-500 self-end  rounded-r-full bg-slate-50 text-slate-950 text-xl">➔</button>
+            <button className="ml-1 px-4 py-2 h-24 w-10 border-l border-b border-orange-500 self-end  rounded-r-full bg-slate-50 text-slate-950 text-xl">➤</button>
 
                
         </form>
