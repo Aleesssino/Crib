9ea7cac19145952f290dfc957be695f9a58eec86
Aleš Gábor <alesg3a1b4o1@gmail.com>
2023-07-04 18:48:44 +0200

    lazy loading

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -8,6 +8,6 @@ type ProfileImageProps = {
 export function ProfileImage({ src, className = ""}:
 ProfileImageProps) {
     return <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
-        {src == null ? null : <Image src={src} alt="Profile Image" quality={100} fill loading="lazy"/>}
+        {src == null ? null : <Image src={src} alt="Profile Image" quality={100} fill loading="eager"/>}
     </div>
 }
