c0b9de101e5ce8d81460cd10f6b881a6e08ead3d
Aleš Gábor <alesg3a1b4o1@gmail.com>
2023-07-05 19:23:41 +0200

    handle input

--- src/components/postForm.tsx
+++ src/components/postForm.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react"
-
+import { FormEvent, useState } from "react";
+import { api } from "~/utils/api";
 export function PostForm () {
     const session = useSession();
 
@@ -15,10 +16,28 @@ export function PostForm () {
 
 
 function Form () {
+    const session = useSession();
+    const [inputValue, setInputValue] = useState("");
+
+    
+    const createPost = api.posts.create.useMutation({
+        onSuccess: (newPost) => {
+          console.log(newPost);
+          setInputValue("");
+        },
+      });
+    
+      function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        createPost.mutate({ content: inputValue });
+      }
+
+
     return (
-        <form className="flex border-b px-4 py-2">
+        <form onSubmit={handleSubmit} className="flex border-b px-4 py-2">
             <div className="relative flex w-full">
-            <textarea placeholder="What's on your mind?" className="bg-transparent flex w-full rounded-xl p-4 text-xl grow border-b-2 overflow-hidden"/>
+            <textarea value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)} placeholder="What's on your mind?" className="bg-transparent flex w-full rounded-xl p-4 text-xl grow border-b-2 overflow-hidden"/>
             
             <button className="ml-8 h-10 w-10 absolute right-0 top-1/2 transform -translate-y-1/2 border-l border-b border-orange-500 self-end  rounded-full bg-slate-50 text-slate-950 text-xl">▲</button>
             
