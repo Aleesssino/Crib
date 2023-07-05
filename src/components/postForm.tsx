e8cbaa6f938ef22a0c44f790c08f21ff415e1fd2
Aleš Gábor <alesg3a1b4o1@gmail.com>
2023-07-05 18:15:21 +0200

    ahh... changing button style ..again

--- src/components/postForm.tsx
+++ src/components/postForm.tsx
@@ -20,7 +20,7 @@ function Form () {
             <div className="relative flex w-full">
             <textarea placeholder="What's on your mind?" className="bg-transparent flex w-full rounded-xl p-4 text-xl grow border-b-2 overflow-hidden"/>
             
-            <button className="pr-8 h-10 w-10 absolute right-0 top-1/2 transform -translate-y-1/2 border-l border-b border-orange-500 self-end  rounded-full bg-slate-50 text-slate-950 text-xl items-center justify-center">🡹</button>
+            <button className="mr-8 h-10 w-10 absolute right-0 top-1/2 transform -translate-y-1/2 border-l border-b border-orange-500 self-end  rounded-full bg-slate-50 text-slate-950 text-xl">🡹</button>
             
             </div>
                
