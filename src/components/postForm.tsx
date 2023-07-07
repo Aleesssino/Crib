05cb4776de29238523675529b90bc79937766978
Aleš Gábor <alesg3a1b4o1@gmail.com>
2023-07-07 20:16:46 +0200

    adding InfinitePosts

--- src/components/postForm.tsx
+++ src/components/postForm.tsx
@@ -39,7 +39,7 @@ function Form () {
             <textarea value={inputValue}
           onChange={(e) => setInputValue(e.target.value)} placeholder="What's on your mind?" className="bg-transparent flex w-full rounded-xl p-4 text-xl grow border-b-2 overflow-hidden"/>
         
-            <button className="ml-8 h-10 w-10 absolute right-0 top-1/2 transform -translate-y-1/2 border-l border-b border-orange-500 self-end  rounded-full bg-slate-50 text-slate-950 text-xl">▶</button>
+            <button className="ml-8 h-10 w-10 absolute right-0 top-1/2 transform -translate-y-1/2 border-l border-b border-orange-500 self-end  rounded-full bg-slate-50 text-slate-950 text-xl">➤</button>
             
             </div>
                
